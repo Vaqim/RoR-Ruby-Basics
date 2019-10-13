dc080a5b00c9034ee70e95309d0d57588af6536b
Vaqim <vadimser1355@gmail.com>
2019-10-13 16:55:43 +0300

    Excercises 1-12

--- 1 Lesson/1.rb
+++ 1 Lesson/1.rb
@@ -1,1 +1,6 @@
-puts "Hello, RoR!"
+# Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.
+# Done by Vadim
+
+array = [rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50)]
+puts "Start array: " + array.to_s
+puts "Result:      " + (array.select.with_index { |_, index| index.even? } + array.select.with_index { |_, index| index.odd? }).to_s
---
+++ 1 Lesson/8.rb
@@ -0,0 +1,7 @@
+#Дан целочисленный массив. Преобразовать его, прибавив к нечетным числам первый элемент. Первый и последний элементы массива не изменять.
+#Done by Vadim
+
+a = [rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50)]
+
+puts "Start array: " + a.to_s
+puts "Result:      " + a.map!.with_index{|e, index| (e.odd? && !(index == 0 || index == a.index(a.last))) ? e += a.first : e}.to_s
