de6006ed7e448b7585ac603a450e28919478728b
Vaqim <vadimser1355@gmail.com>
2019-10-16 19:42:29 +0300

    correction in 4

--- 1 Lesson/4.rb
+++ 1 Lesson/4.rb
@@ -1,6 +1,6 @@
 #Дан целочисленный массив. Вывести номер последнего из тех его элементов, которые удовлетворяют двойному неравенству: A[0] < A[i] < A[-1]. Если таких элементов нет, то вывести [ ].
 #Done by Vadim
 
-a = [rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0), rand(-2.0..2.0)]
+a = [rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50), rand(50)]
 puts "Start array: " + a.to_s
-puts "Result:      " + a.index(a.select {|e| (e < 0) && (e > -1)}.last).to_s
+puts "Result:      " + a.index(a.select {|e| (e < a[0]) && (e > a[-1])}.last).to_s
