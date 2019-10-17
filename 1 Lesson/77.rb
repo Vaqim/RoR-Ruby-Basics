3d348d09ddce0548f6a1e1e621cee83cd6169374
Vaqim <vadimser1355@gmail.com>
2019-10-17 14:58:48 +0300

    correction in 77.rb

--- 1 Lesson/77.rb
+++ 1 Lesson/77.rb
@@ -1,5 +1,5 @@
 # Дано целое число. Найти сумму его цифр.
 
-num, sum = gets.chomp.to_i, 0
-num.to_s.split('').each{|e| sum += e.to_i}
+num, sum = gets.chomp, 0
+num.split('').each{|e| sum += e.to_i}
 puts sum.to_s
